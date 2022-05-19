15c9094d88aecfb13775d29bb727f2036a381715
nourah <altamiminourahcs@gmail.com>
2022-05-19 11:00:00 +0300

    lab11

---
+++ lab11.py
@@ -0,0 +1,10 @@
+
+def additoin(x, y):
+     print("Addition:", x +b)     
+try:
+    additoin(10, 20)
+except Exception as ve:
+    print(ve.__class__)
+else:
+    print("the operation is successful")
+    
