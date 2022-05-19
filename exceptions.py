fa1095ab9ddfe7eeffeb715620dd81aeed6d0f77
RubaALmohya <ruba12399@gmail.com>
2022-05-19 10:35:12 +0300

    solved

---
+++ exceptions.py
@@ -0,0 +1,11 @@
+def additoin(x, y):
+    x = 10
+    y = 20
+    try:
+        print("Addition:", x + b)
+    except NameError as ne:
+        print("NameError occurred: \nSome variable isn't defined \ncheck your variable Please")
+    else:
+        print("the operation is successful")
+additoin(10, 20)
+
