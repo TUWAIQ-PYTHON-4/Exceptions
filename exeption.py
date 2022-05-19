34b077e6544bcd5a4428a48ec61f392db24ca85a
ahmed <a-5amd@hotmail.com>
2022-05-19 10:59:56 +0300

    exeption lab

---
+++ exeption.py
@@ -0,0 +1,12 @@
+def additoin(x, y):
+    x = 10
+    y = 20
+    print("Addition:", x + b)
+
+
+
+
+try:
+    additoin(10, 20)
+except NameError :
+    print( "name 'b' is not defined please define")
