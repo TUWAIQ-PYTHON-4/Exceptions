4d61107560d2fbaaece38c0821d00d29ad6b309e
jawaher-alqotym <jwahralqtym@gmail.com>
2022-05-19 10:31:01 +0300

    add one file

---
+++ exceptions.py
@@ -0,0 +1,13 @@
+def additoin(x, y):
+          x = 10
+          y = 20
+          print("Addition:", x + b)
+
+
+
+try:
+  additoin(10, 20)
+except Exception as e:
+    print(e.__class__)
+else:
+    print("the operation is successful")
