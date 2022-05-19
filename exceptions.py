f1dad6b6687f4c13f4a15cffd772012508cee00a
rahaf-adel <rahafadeel5@gmail.com>
2022-05-19 10:59:45 +0300

    Edit the Exceptions Lab

--- exceptions.py
+++ exceptions.py
@@ -1,19 +1,23 @@
 def additoin(x, y):
           x = 10
           y = 20
-          raise NameError ("You use not definde varible")
           print("Addition:", x + b)
+         
+            
+          
+          
+          
+
+
 
 try:
     additoin(10, 20)
 except NameError as n:
-    print(n)
+    print("You use not definde varible")
 else:
     print("the operation is successful")
-finally:
-    print("Run in all cases on failuer or success")
+
+
 
 #additoin(10, 20)
-
-
 
