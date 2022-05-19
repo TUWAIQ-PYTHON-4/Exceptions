a5426962ac5460e58e82fa8a1293debcb946f3af
noura <noura@gmail.com>
2022-05-19 11:09:23 +0300

    lab Exception

---
+++ lab_Exceptions.py
@@ -0,0 +1,52 @@
+'''
+# Exceptions
+
+
+## Below you have a code that raises an exception , using what you learned do the following:
+- Find what type of exception is raised.
+- Hanlde the exception in try..except 
+- If operation successful , print "the operation is successful"
+- if operation fails, handle the specific exception that is raised , and print a relevant message.
+
+      def additoin(x, y):
+          x = 10
+          y = 20
+          print("Addition:", x + b)
+
+
+      additoin(10, 20)
+
+'''
+
+'''
+
+def additoin(x, y):
+        try:
+           x = 10
+           y = 20
+           print("Addition:" , x + y)
+        except operation_fails as a:
+            print(a)
+        else:
+            print("the operation is successful")
+        
+'''
+
+from decimal import DivisionByZero
+
+
+def additoin(x, y):
+    x = 10
+    y = 20
+    print("Addition:", x + b)
+
+try:
+   additoin(10, 0)
+except NameError as ne:
+    print("the opration is fail becouse you have name error")
+except Exception as er:
+    print(er.__class__)
+else:
+    print("the operation is successful")
+
+ 
