06bf38ce211a1de64d5ab4e4e8882de1f2d8d7e4
Emtinan98 <102264151+Emtinan98@users.noreply.github.com>
2022-05-19 11:11:56 +0300

    Exception_lab Update

--- excption_lab.py
+++ excption_lab.py
@@ -1,23 +1,15 @@
-class ValueError(Exception):
 
- def addition(x, y):
-    x = 10
-    y = 20
-    print("Addition:", x + y)
-    raise ValueError("please enter a int value!")
+def additoin(x, y):
+          x = 10
+          y = 20
+          print("Addition:", x + b)
 
 
- try:
-    addition('e','r')
- except Exception as e:
-    print(e.__class__)
- finally:
+
+try :
+    additoin(10, 20)
+except NameError as e:
+    print("the operation is not allowed",e)
+else:
     print("the operation is successful")
-
-    addition('e','e')
-
-
-
-
-
 
